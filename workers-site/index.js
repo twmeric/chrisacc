ab4d374ded0147d0a03ff22152d26ed2145594fb
twmeric <twmeric@gmail.com>
2026-04-10 17:55:05 +0800

    Update Worker: Point to new Pages URL b2a13409
    - New Pages deployment: https://b2a13409.ltcpa-website.pages.dev
    - Includes CMS Admin v6.0 with full editing capabilities

--- workers-site/index.js
+++ workers-site/index.js
@@ -7,7 +7,7 @@ export default {
       return Response.redirect('https://www.ltgroupcpa.com' + url.pathname + url.search, 301);
     }
     
-    const targetUrl = 'https://71d7e7b7.ltcpa-website.pages.dev' + url.pathname + url.search;
+    const targetUrl = 'https://b2a13409.ltcpa-website.pages.dev' + url.pathname + url.search;
     
     const modifiedRequest = new Request(targetUrl, {
       method: request.method,
